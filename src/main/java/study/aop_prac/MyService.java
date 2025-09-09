bfd55121194684a6dac03d7b47c5ea5af6651a92
swlee <swo98@naver.com>
2025-09-09 20:12:10 +0900

    fix: 무의미 로그 지우고 system.out을 log.info로 변경

--- src/main/java/study/aop_prac/MyService.java
+++ src/main/java/study/aop_prac/MyService.java
@@ -9,7 +9,6 @@ public class MyService {
 
     @LogExecutionTime
     public void callExternal() {
-        log.info("MyService.callExternal() 실행 | 실제 클래스: {}", this.getClass().getName());
         // 이 메소드는 외부(CommandLineRunner) 로부터 호출됨
         // 외부에서 호출되므로 프록시를 거치게 되고, 따라서 AOP가 정상적으로 동작할 것임
         System.out.println("callExternal() 메소드 실행");
@@ -23,7 +22,6 @@ public class MyService {
 
     @LogExecutionTime
     public void callInternal() {
-        log.info("MyService.callInternal() 실행 | 실제 클래스: {}", this.getClass().getName());
         // 이 메소드는 callExternal() 내부에서 this를 통해 호출됨
         // 내부 호출이므로 프록시를 우회하게 되어 AOP가 동작하지 않을 것
         System.out.println("callInternal() 메소드 실행");
