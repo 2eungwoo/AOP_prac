92e8655b4235c7862ad4d9cc242ca40d0c827b28
swlee <swo98@naver.com>
2025-09-09 18:03:01 +0900

    feat: PerfAspect 로깅 AOP 정의

---
+++ src/main/java/study/aop_prac/PerfAspect.java
@@ -0,0 +1,32 @@
+package study.aop_prac;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class PerfAspect {
+
+    @Pointcut("@annotation(study.aop_prac.LogExecutionTime)")
+    private void logExecutionTimePointcut() {}
+
+    @Around("logExecutionTimePointcut()")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+
+        Object result = joinPoint.proceed();
+
+        long endTime = System.currentTimeMillis();
+        long executionTime = endTime - startTime;
+
+        String methodName = joinPoint.getSignature().getName();
+        log.info("[ExecutionTime] Method: {} executed in {} ms", methodName, executionTime);
+
+        return result;
+    }
+}
