298d59c89259b23ef55a93ca713cf6f35f7979ab
swlee <swo98@naver.com>
2025-09-09 18:07:25 +0900

    add: AOP 내부 호출 문제 재현을 위한 코드 추가

--- src/main/java/study/aop_prac/MyService.java
+++ src/main/java/study/aop_prac/MyService.java
@@ -7,9 +7,20 @@ public class MyService {
 
     @LogExecutionTime
     public void doSomething() {
+        System.out.println("doSomething() 메소드 실행");
         try {
-            // 1초간 대기하여 실행 시간을 인위적으로 만듦
             Thread.sleep(1000);
+            this.doSomethingElse(); // 내부 호출
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @LogExecutionTime
+    public void doSomethingElse() {
+        System.out.println("doSomethingElse() 메소드 실행");
+        try {
+            Thread.sleep(500);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
