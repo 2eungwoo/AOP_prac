f0c3a5e5a62c4de6c6d5299c6fb7ff2875f70251
swlee <swo98@naver.com>
2025-09-09 18:03:51 +0900

    add: AOP 테스트를 위한 CommandLineRunner 추가

--- src/main/java/study/aop_prac/AopPracApplication.java
+++ src/main/java/study/aop_prac/AopPracApplication.java
@@ -1,13 +1,22 @@
 package study.aop_prac;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AopPracApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(AopPracApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(AopPracApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner run(MyService myService) {
+		return args -> {
+			myService.doSomething();
+		};
+	}
 
 }
